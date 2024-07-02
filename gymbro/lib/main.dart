adaf91d1e49f54701e19c8dbee40bd250265226c
EnzoGuillouche <enzo.guillouche@algosup.com>
2024-07-02 18:07:23 +0200

    Fixed issues with notifications

--- gymbro/lib/main.dart
+++ gymbro/lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'routes.dart';
+import 'preferences.dart';
+import 'notif.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    initializeNotifications();
+
+    if (isPrWeek()) {
+      showNotification();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
